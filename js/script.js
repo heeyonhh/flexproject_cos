a5215eecff89a978f55b35cc57e5a7f01b1fe4ac
heeyonhh <lightram23@gmail.com>
2023-07-25 16:38:19 +0900

    index

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.nav>ul>li').mouseover(function(){
+        $(this).find('.depth2').addClass('active');
+        $('.menueffect').addClass('active');
+    });
+    $('.nav>ul>li').mouseout(function(){
+        $(this).find('.depth2').removeClass('active');
+        $('.menueffect').removeClass('active');
+    });
+});
