af0925c07850494aed26a377db1a7879578045ce
heeyonhh <lightram23@gmail.com>
2023-07-28 16:53:58 +0900

    mobile menu

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ $(function(){
         $('.menueffect').removeClass('active');
     });
 });
+
+//모바일 메뉴
+$(function(){
+    $('.mobilenav>li').mouseover(function(){
+        $(this).find('.mobiledepth2').show();
+    });
+    $('.mobilenav>li').mouseout(function(){
+        $(this).find('.mobiledepth2').hide();
+    });
+
+    // 메뉴 쇼 하이드
+    $('.ham').click(function(){
+        $('.mobilebg').show();
+        $('.bi-x-lg').click(function(){
+            $('.mobilebg').hide();
+        });
+    })
+
+})
